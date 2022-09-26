b717b14d31eed54d89d7e6a69f7bd2a5355b03da
FernandoPestillo <fernandopestillo@gmail.com>
2022-09-26 09:40:03 -0300

    CRUD completo para cliente + mudança na home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,10 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
 import { IConfig, NgxMaskModule } from 'ngx-mask';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
+import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 
 
 export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
@@ -53,7 +57,11 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
     LoginComponent,
     TecnicoCreateComponent,
     TecnicoUpdateComponent,
-    TecnicoDeleteComponent
+    TecnicoDeleteComponent,
+    ClienteListComponent,
+    ClienteCreateComponent,
+    ClienteUpdateComponent,
+    ClienteDeleteComponent
   ],
   imports: [
     BrowserModule,
